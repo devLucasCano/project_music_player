5b83eaf415a1a4c1e7f69aa73042fd581716c6cc
Lucas Cano <101060147+devLucasCano@users.noreply.github.com>
2022-10-09 19:57:57 -0300

    AroundYou

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -9,12 +9,40 @@ export const shazamCoreApi = createApi({
         "X-RapidAPI-Key",
         "3c1a2300fdmshb768080d5614752p1897fdjsn2fd612f4926d"
       );
+
       return headers;
     },
   }),
-  endpoints: (biulder) => ({
-    getTopCharts: biulder.query({ query: () => "/charts/world" }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `/charts/genre-world?genre_code=${genre}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `/charts/country?country_code=${countryCode}`,
+    }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) => `/artists/details?artist_id=${artistId}`,
+    }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `/tracks/related?track_id=${songid}`,
+    }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const {
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
+  useGetArtistDetailsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} = shazamCoreApi;
