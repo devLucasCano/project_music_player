aef85a55a35df1508523a2ef6eed5f0b63f9529b
Lucas Cano <101060147+devLucasCano@users.noreply.github.com>
2022-10-09 21:54:13 -0300

    search

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -57,7 +57,6 @@ const Sidebar = () => {
           />
         )}
       </div>
-      -
       <div className="absolute md:hidden block top-6 right-3">
         {!mobileMenuOpen ? (
           <HiOutlineMenu
@@ -71,7 +70,6 @@ const Sidebar = () => {
           />
         )}
       </div>
-      -
       <div
         className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
           mobileMenuOpen ? "left-0" : "-left-full"
